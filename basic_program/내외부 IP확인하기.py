c4a9b11dcc2a17f23835a4567b974534f2e5cbca
jsk890 <resqme89@naver.com>
2022-05-16 15:06:12 +0900

    basic_program

--- basic_program/내외부 IP확인하기.py
+++ basic_program/내외부 IP확인하기.py
@@ -13,12 +13,12 @@ in_addr = socket.gethostbyname(socket.gethostname())
 in_addr2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 in_addr2.connect(("www.google.co.kr",443))
 # 내부 IP확인
-print(in_addr)
-print(in_addr2.getsockname()[0])
+print("내부 IP: ",in_addr)
+print("내부 IP(외부사이트 접속): ",in_addr2.getsockname()[0])
 
 # IPconfig 사이트에 접속
 req = requests.get("http://ipconfig.kr")
 # 정규식 표현을 사용하여 IP주소를 가져옴
 out_addr = re.search(r'IP Address : (\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3})', req.text)[1]
 # 외부 IP확인
-print(out_addr)
+print("외부 IP: ",out_addr)
